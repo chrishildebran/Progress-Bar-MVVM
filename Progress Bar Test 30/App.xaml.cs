12382827dc63130d3b4a465256d399ac766533c4
Chris Hildebran <chrishildebran@gmail.com>
2021-03-26 07:05:13 -0700

    Commit2

--- Progress Bar Test 30/App.xaml.cs
+++ Progress Bar Test 30/App.xaml.cs
@@ -4,7 +4,7 @@
 // Solution:............ Progress Bar Test 30
 // Project:............. ProgressBar
 // File:................ App.xaml.cs
-// Last Code Cleanup:... 03/26/2021 @ 6:53 AM Using ReSharper
+// Last Code Cleanup:... 03/26/2021 @ 7:04 AM Using ReSharper
 // Review Comment:...... // ✓✓ 03/26/2021 - Review Comment:
 // ///////////////////////////////////////////////////////////
 
@@ -20,6 +20,8 @@ namespace ProgressBar
         #region Properties (Non-Private)
 
         public string Property1 { get; set; }
+
+        public string Property2 { get; set; }
 
         #endregion
     }
